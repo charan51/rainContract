9c21b1047e839b0f24615fe9c36ab74edf3170be
charan51 <cvcharan51@gmail.com>
2019-11-25 18:45:14 +0530

    updated

--- cropDamange.sol
+++ cropDamange.sol
@@ -15,7 +15,9 @@ contract CropDamage {
    
     // Events List
     event CallForRegister(string);
+    event RejectUserRegister(string, uint);
     event verifyKissanNumber(uint);
+    event sendCropAnyalsis(string, uint, uint);
     // To check if user exists on the contract
     function checkUser(uint _kissanNumber) public returns(bool) {
         bool exists = Iterator.getElement(knownValues, _kissanNumber) != address(0);
@@ -37,20 +39,32 @@ contract CropDamage {
         if(valid) {
         Iterator.put(knownValues, msg.sender, _name, _location, _kissanNumber);
         } else {
-            // reject();
+            // reject user for invalid kissan Number;
+            emit RejectUserRegister('Invalid Kissan Number, Please verifiy with valid number', _kissanNumber);
         }
     }
-   
+//   Adverse weather conditions
+// Fire
+// Insects*
+// Plant disease*
+// Wildlife
+// Earthquake
+// Volcanic eruption
+// Failure of the irrigation water supply, if applicable, due to an unavoidable cause of loss occurring within the insurance period.
     // Apply for new policy
     function check(uint _number) public returns(Iterator.Status) {
         claimState = Iterator.getClaimStatus(knownValues, _number);
     }
-    function applyPolicy() public view returns(Iterator.Status) {
-       
-       
-        if(claimState == Iterator.Status.INACTIVE) {
-            return claimState;
+    function applyPolicy(string memory _cropName, uint _cropPrice, uint _landArea, uint _kissanNumber) public returns(Iterator.Status) {
+        if(claimState == Iterator.Status.INACTIVE || claimState == Iterator.Status.CANCLED) {
+            // Send details to oracle to calculate the crop risk premium
+           emit sendCropAnyalsis(_cropName, _cropPrice, _landArea, _kissanNumber);
         }
-       
+    }
+    
+    // Once the oracle confirmed risk as user to pay 
+    
+    function payForPolicy(uint _kissanNumber, uint _premium) public payable {
+        require(msg.value == _premium)
     }
 }
