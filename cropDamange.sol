60149fb035b71cf8adb6caa3ca714e358852d8eb
charan51 <cvcharan51@gmail.com>
2019-12-04 09:12:03 +0530

    updated1

--- cropDamange.sol
+++ cropDamange.sol
@@ -4,10 +4,11 @@ contract CropDamagev1 {
     address private admin;
     Iterator.Status ClaimStatus;
     Iterator.Data farmValues;
-    constructor() public {
+    uint public poolFunds;
+    constructor() public payable {
+        poolFunds = msg.value;
         admin = msg.sender;
     }
-    
     // Modifiers 
     modifier onlyAdmin() {
         require(msg.sender == admin, 'Only admin has access');
@@ -24,48 +25,58 @@ contract CropDamagev1 {
     }
     modifier claimActive(uint _kissanNumber) {
         Iterator.Status status = Iterator.getClaimStatus(farmValues, _kissanNumber);
-        require(status == Iterator.Status.ACTIVE, "policy already bought!");
+        require(status == Iterator.Status.ACTIVE, "policy already bought or policy inactive!");
         _;
     }
     // events
     event farmerActionStatus(string);
-    
+    event farmerActionValue(uint);
     // function 
     function addFarmer(uint _kissanNumber, string memory _name, string memory _location) public onlyFarmer returns(bool) {
+        if(_kissanNumber != 0 && bytes(_name).length != 0 && bytes(_location).length != 0) {
         bool exists = farmValues.farmerList[_kissanNumber].farmerAddress != address(0);
         if(!exists) {
-            Iterator.add(farmValues, address(msg.sender), _name, _location, _kissanNumber);
             emit farmerActionStatus("Farmer register successfully");
+            Iterator.add(farmValues, address(msg.sender), _name, _location, _kissanNumber);
             return true;
+        } 
         } else {
-            emit farmerActionStatus("Farmer register failed, already registered!!!");
+              emit farmerActionStatus("Farmer register failed, already registered!!!");
             return false;
         }
     }
-    // Buy crop policy
-    
-    function buyPolicy(uint _kissanNumber, uint _cropPrice, uint _landArea, string memory _cropLocaton) public onlyFarmer claimInActive(_kissanNumber) payable {
-        
-        if(_kissanNumber != 0 && _cropPrice != 0 && _landArea != 0 && bytes(_cropLocaton).length != 0){
-        uint PremiumCost = (uint(6000)/uint(100))*(uint(_cropPrice)*uint(_landArea));
+    // calulate the policy crop policy
+    function calculatePremium(uint _cropPrice, uint _landArea) public pure returns (uint, uint) {
+        uint PremiumCost = (uint(60)*uint(_cropPrice)*uint(_landArea))/(uint(100));
         uint totalPremiumCost = (uint(_cropPrice)*uint(_landArea)) - PremiumCost;
         uint totalCoverage =  uint(_cropPrice)*uint(_landArea);
-        require(msg.value == totalPremiumCost, 'Premium amount paid too less');
-        farmValues.farmerList[_kissanNumber].premium = totalPremiumCost;
-        farmValues.farmerList[_kissanNumber].coverage = totalCoverage;
+        return (totalPremiumCost, totalCoverage);
+    }
+    // buyPolicy for crop
+    function buyPolicy(uint _kissanNumber, uint _cropPrice, uint _landArea, uint _premiumCost, uint _coverageCost, string memory _cropLocaton) public onlyFarmer claimInActive(_kissanNumber) payable {
+        if(_kissanNumber != 0 && _cropPrice != 0 && _landArea != 0 && bytes(_cropLocaton).length != 0){
+        require(msg.value == _premiumCost, 'Premium amount paid too less');
+        emit farmerActionStatus("Farmer purchased policy successfully");
+        farmValues.farmerList[_kissanNumber].premium = _premiumCost;
+        farmValues.farmerList[_kissanNumber].coverage = _coverageCost;
         farmValues.farmerList[_kissanNumber].landAcers = _landArea;
         farmValues.farmerList[_kissanNumber].status = Iterator.Status.ACTIVE;
         farmValues.farmerList[_kissanNumber].cropLocation = _cropLocaton;
-        farmValues.farmerList[_kissanNumber].policyNumber +=1;
-        emit farmerActionStatus("Farmer purchased policy successfully");
         } else {
             emit farmerActionStatus("Farmer failed to purchase, payment reverted");
-            revert();
+            revert("Policy already activated");
         }
     }
-    function checkElement(uint _kissanNumber) public view returns(Iterator.Status) {
-        return Iterator.getClaimStatus(farmValues, _kissanNumber);
+    // claim for policy
+    function claim_for_damage(uint _kissanNumber, string memory _reason) public onlyFarmer claimActive(_kissanNumber) {
+        if(address(farmValues.farmerList[_kissanNumber].farmerAddress) == msg.sender){
+            emit farmerActionStatus("farmer claimed successfully");
+            farmValues.farmerList[_kissanNumber].status = Iterator.Status.INACTIVE;
+            farmValues.farmerList[_kissanNumber].totalClaim++;
+            farmValues.farmerList[_kissanNumber].claimReason = _reason;
+            address(msg.sender).transfer(farmValues.farmerList[_kissanNumber].coverage);   
+        } else {
+            emit farmerActionStatus('Invalid farmer address');
+        }
     }
-    
-
 }
